44dba547d2c256db79d80b6e8c26a3052ac5e452
Stacey Li <listacey22@yahoo.com>
2021-05-22 17:35:46 -0400

    new items to campus model

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -1,12 +1,26 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
+// add 2 new items: imageUrl - with a default value,
+// address - not empty or null
+
 const Campus = db.define("campus", {
 
   name: {
     type: Sequelize.STRING,
     allowNull: false
   },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://rimatour.com/wp-content/uploads/2017/09/No-image-found.jpg"
+  },
+
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
 
   description: {
     type: Sequelize.STRING,
