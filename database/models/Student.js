fc80815daadf65b8dc54a75b3571d10aba2583ce
Stacey Li <listacey22@yahoo.com>
2021-05-22 18:39:58 -0400

    student model added with dummy data

--- database/models/Student.js
+++ database/models/Student.js
@@ -3,6 +3,10 @@ const db = require('../db');
 
 const Student = db.define("student", {
 
+// added email - not empty or null; must be a valid email
+  // - [ ] imageUrl - with a default value
+  // - [ ] gpa - decimal between 0.0 and 4.0
+
   firstname: {
     type: Sequelize.STRING,
     allowNull: false
@@ -11,6 +15,29 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: "https://rimatour.com/wp-content/uploads/2017/09/No-image-found.jpg"
+  },
+
+  gpa: {
+    type: Sequelize.DOUBLE(1,1),
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 4
+    }
   }
 
 });
